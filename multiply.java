3d4b477505161056334104ca2bb3c84e93ecd327
[kannya rajendra bhoir] <[kanyabhoir2209@gmail.com]>
2022-05-05 04:39:56 -0700

    test

--- multiply.java
+++ multiply.java
@@ -3,7 +3,7 @@ public class multiply{
  public static void main(String[] args){
 
         var b = 20;
-        var a = 5;
+        var a = 5;    
 
         system.out.println("The sum of"+(a+b))
     }
