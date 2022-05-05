88b9a42ba2a528b3cd4ddae7c6367c7e7be3d847
[kannya rajendra bhoir] <[kanyabhoir2209@gmail.com]>
2022-05-05 04:48:30 -0700

    veriable change in multiply.java

--- multiply.java
+++ multiply.java
@@ -2,10 +2,10 @@ public class multiply{
 
  public static void main(String[] args){
 
-        var b = 20;
+        var c = 20;
         var a = 5;    
     
-        system.out.println("The sum of"+(a+b))
+        system.out.println("The sum of"+(a+c))
     }
 
 }
